7ab93fec0952d71555bd10eac9e055fee0cb92f3
anilyuo <168365194+anilyuo@users.noreply.github.com>
2024-05-23 00:08:04 +0530

    Create man.tf

---
+++ terrafrom/man.tf
@@ -0,0 +1,45 @@
+resource "aws_vpc" "vpc" {
+    cidr_block              = var.vpc_cidr
+    enable_dns_hostnames    = true
+    enable_dns_support      = true
+}
+
+resource "aws_internet_gateway" "igw" {
+    vpc_id                  = aws_vpc.vpc.id
+}
+
+
+resource "aws_subnet" "public_subnet_1" {
+    vpc_id                  = aws_vpc.vpc.id
+    cidr_block              = var.public_subnet_1
+    availability_zone       = var.availibilty_zone_1
+    map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "public_subnet_2" {
+    vpc_id                  = aws_vpc.vpc.id
+    cidr_block              = var.public_subnet_2
+    availability_zone       = var.availibilty_zone_2
+    map_public_ip_on_launch = true
+}
+
+resource "aws_route_table" "public" {
+    vpc_id                  = aws_vpc.vpc.id
+    
+}
+
+resource "aws_route" "public" {
+    route_table_id          = aws_route_table.public.id
+    destination_cidr_block  = "0.0.0.0/0"
+    gateway_id              = aws_internet_gateway.igw.id
+}
+
+resource "aws_route_table_association" "public1" {
+    subnet_id               = aws_subnet.public_subnet_1.id
+    route_table_id          = aws_route_table.public.id
+}
+resource "aws_route_table_association" "public2" {
+    subnet_id               = aws_subnet.public_subnet_2.id
+    route_table_id          = aws_route_table.public.id
+}
+
