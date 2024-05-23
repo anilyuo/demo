ccbe177fb87a60317f3ee63529594dc98203e9dc
anilyuo <168365194+anilyuo@users.noreply.github.com>
2024-05-23 13:40:45 +0530

    Update man.tf

--- terrafrom/man.tf
+++ terrafrom/man.tf
@@ -1,45 +1,67 @@
-resource "aws_vpc" "vpc" {
-    cidr_block              = var.vpc_cidr
-    enable_dns_hostnames    = true
-    enable_dns_support      = true
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  tags = {
+    name = "main"
+  }
+}
+resource "aws_subnet" "subnet" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, 1)
+  map_public_ip_on_launch = true
+  availability_zone       = "eu-central-1a"
 }
 
-resource "aws_internet_gateway" "igw" {
-    vpc_id                  = aws_vpc.vpc.id
+resource "aws_subnet" "subnet2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, 2)
+  map_public_ip_on_launch = true
+  availability_zone       = "eu-central-1b"
 }
 
-
-resource "aws_subnet" "public_subnet_1" {
-    vpc_id                  = aws_vpc.vpc.id
-    cidr_block              = var.public_subnet_1
-    availability_zone       = var.availibilty_zone_1
-    map_public_ip_on_launch = true
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "internet_gateway"
+  }
 }
 
-resource "aws_subnet" "public_subnet_2" {
-    vpc_id                  = aws_vpc.vpc.id
-    cidr_block              = var.public_subnet_2
-    availability_zone       = var.availibilty_zone_2
-    map_public_ip_on_launch = true
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
 }
 
-resource "aws_route_table" "public" {
-    vpc_id                  = aws_vpc.vpc.id
-    
+resource "aws_route_table_association" "subnet_route" {
+  subnet_id      = aws_subnet.subnet.id
+  route_table_id = aws_route_table.route_table.id
 }
 
-resource "aws_route" "public" {
-    route_table_id          = aws_route_table.public.id
-    destination_cidr_block  = "0.0.0.0/0"
-    gateway_id              = aws_internet_gateway.igw.id
+resource "aws_route_table_association" "subnet2_route" {
+  subnet_id      = aws_subnet.subnet2.id
+  route_table_id = aws_route_table.route_table.id
 }
 
-resource "aws_route_table_association" "public1" {
-    subnet_id               = aws_subnet.public_subnet_1.id
-    route_table_id          = aws_route_table.public.id
-}
-resource "aws_route_table_association" "public2" {
-    subnet_id               = aws_subnet.public_subnet_2.id
-    route_table_id          = aws_route_table.public.id
+resource "aws_security_group" "security_group" {
+  name   = "ecs-security-group"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    self        = "false"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "any"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
